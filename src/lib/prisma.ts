77d0ac9c011d6dfa3c6f958252623ff19bbc76c1
Mbaduko <nsengiyumvaclement247@gmail.com>
2026-01-26 22:30:11 +0200

    chore: Apply presave password hashing

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,59 @@
-
 import { PrismaClient } from '@prisma/client';
+import argon from 'argon2';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+    query:{
+        user: {
+            async create({args, query}) {
+                if (args.data.password) {
+                    let plainPassword: string | undefined;
+                    if (typeof args.data.password === 'string') {
+                        plainPassword = args.data.password;
+                    } else if (
+                        typeof args.data.password === 'object' &&
+                        args.data.password !== null &&
+                        'set' in args.data.password &&
+                        typeof (args.data.password as any).set === 'string'
+                    ) {
+                        plainPassword = (args.data.password as any).set;
+                    }
+                    if (plainPassword) {
+                        const hashed = await argon.hash(plainPassword, { type: argon.argon2id });
+                        if (typeof args.data.password === 'string') {
+                            args.data.password = hashed;
+                        } else {
+                            (args.data.password as any).set = hashed;
+                        }
+                    }
+                }
+                return query(args);
+            },
+            async update({args, query}) {
+                if (args.data.password) {
+                    let plainPassword: string | undefined;
+                    if (typeof args.data.password === 'string') {
+                        plainPassword = args.data.password;
+                    } else if (
+                        typeof args.data.password === 'object' &&
+                        args.data.password !== null &&
+                        'set' in args.data.password &&
+                        typeof (args.data.password as any).set === 'string'
+                    ) {
+                        plainPassword = (args.data.password as any).set;
+                    }
+                    if (plainPassword) {
+                        const hashed = await argon.hash(plainPassword, { type: argon.argon2id });
+                        if (typeof args.data.password === 'string') {
+                            args.data.password = hashed;
+                        } else {
+                            (args.data.password as any).set = hashed;
+                        }
+                    }
+                }
+                return query(args);
+            }
+        }
+    }
+});
 
 export default prisma;
